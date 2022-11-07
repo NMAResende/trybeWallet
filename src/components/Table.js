82d5d1e88a886e990493dbd733c165da817f4b2f
Nathália <nathaliamarciaandrade@gmail.com>
2022-11-07 18:01:44 -0300

    Finalizando CSS

--- src/components/Table.js
+++ src/components/Table.js
@@ -22,13 +22,21 @@ class Table extends Component {
           <thead>
             <tr>
               <th>Descrição</th>
+              <th>|</th>
               <th>Tag</th>
+              <th>|</th>
               <th>Método de pagamento</th>
+              <th>|</th>
               <th>Valor</th>
+              <th>|</th>
               <th>Moeda</th>
+              <th>|</th>
               <th>Câmbio utilizado</th>
+              <th>|</th>
               <th>Valor convertido</th>
+              <th>|</th>
               <th>Moeda de conversão</th>
+              <th>|</th>
               <th>Editar/Excluir</th>
             </tr>
           </thead>
@@ -36,18 +44,26 @@ class Table extends Component {
             { expenses.map((coin) => (
               <tr key={ coin.id }>
                 <td>{ coin.description }</td>
+                <td>|</td>
                 <td>{ coin.tag }</td>
+                <td>|</td>
                 <td>{coin.method }</td>
+                <td>|</td>
                 <td>{ (+coin.value).toFixed(2) }</td>
+                <td>|</td>
                 <td>{ coin.exchangeRates[coin.currency].name }</td>
+                <td>|</td>
                 <td>
                   {
                     (+coin.exchangeRates[coin.currency].ask * +coin.value)
                       .toFixed(2)
                   }
                 </td>
+                <td>|</td>
                 <td>{ (+coin.exchangeRates[coin.currency].ask).toFixed(2) }</td>
+                <td>|</td>
                 <td>Real</td>
+                <td>|</td>
                 <td>
                   <button
                     type="button"
